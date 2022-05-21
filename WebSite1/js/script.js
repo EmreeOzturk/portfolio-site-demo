0568501f12d27aef0cd96c9bb5f36fcddda93fac
Emre Ozturk <88751167+developer-emre@users.noreply.github.com>
2022-05-21 21:16:08 +0300

    Add files via upload

---
+++ WebSite1/js/script.js
@@ -0,0 +1,8 @@
+// typing animation
+
+var typed = new Typed(".typing",{
+    strings:["Fullstack Developer","Web Developer","Java Master :)","Web3 Developer","Blockchain Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop : true
+})
